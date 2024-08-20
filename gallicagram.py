a2a07e874688052d540fa500747b3f71cc09d73f
Benjamin AZOULAY <25954316+benjyazoulay@users.noreply.github.com>
2024-08-20 18:17:06 +0200

    Update gallicagram.py

--- gallicagram.py
+++ gallicagram.py
@@ -96,7 +96,9 @@ def obtenir_donnees_gallicagram(terme, debut, fin, resolution, corpus):
         st.error("Erreur lors de la récupération des données depuis l'API")
         return None
 st.markdown("""
-    <h1> <a href="https://gallicagram.streamlit.app/" target="_self" style="color: inherit; text-decoration: none;">Gallicagram</a> </h1>
+    <a href="https://gallicagram.streamlit.app/" target="_self">
+        <img src="(https://github.com/user-attachments/assets/58e05d4b-04de-45c7-8bbc-5e69e76ecfd4" alt="Gallicagram" style="width: 200px;"/>
+    </a>
     """, unsafe_allow_html=True)
 # Fonction pour lancer la recherche
 def lancer_recherche():
